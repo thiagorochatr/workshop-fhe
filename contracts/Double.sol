afa8d81a444869189bcc15e7fefbecf7287a2ff4
thiagorochatr <thiagord2016@gmail.com>
2025-02-24 18:50:33 -0300

    slide 6

--- contracts/Double.sol
+++ contracts/Double.sol
@@ -1,24 +1,29 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
+import "fhevm/lib/TFHE.sol";
+import "fhevm/config/ZamaFHEVMConfig.sol";
+
 pragma solidity ^0.8.24;
 
-contract Double {
-    uint256 _number;
-    uint256 _double;
+contract Double is SepoliaZamaFHEVMConfig {
+    euint256 private _number;
+    euint256 private _double;
 
     function setNumber(uint256 number_) public {
-        _number = number_;
+        _number = TFHE.asEuint256(number_);
+        TFHE.allowThis(_number);
     }
 
-    function getNumber() public view returns (uint256) {
+    function getNumber() public view returns (euint256) {
         return _number;
     }
 
-    function getDoubleNumber() public view returns (uint256) {
+    function getDoubleNumber() public view returns (euint256) {
         return _double;
     }
 
     function doubleNumber() public {
-        _double = _number * 2;
+        _double = TFHE.mul(_number, 2);
+        TFHE.allowThis(_double);
     }
 }
